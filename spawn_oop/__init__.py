5eb724c70ce85e81f8eb238742830a139c20dde0
Eduard Carreras <ecarreras@gisce.net>
2011-11-09 13:00:53 +0100

    IMP Funcionar a través d'SSL
    Codereview: http://cr.gisce.net/7038050/

--- spawn_oop/__init__.py
+++ spawn_oop/__init__.py
@@ -10,6 +10,7 @@ from ooop import OOOP, Manager
 
 import ir_cron
 import netsvc
+from tools import config
 
 __version__ = '0.3.2'
 
@@ -51,8 +52,11 @@ def spawn(port=8069):
                 user = user_obj.browse(cursor, uid, uid).login
                 pwd = user_obj.browse(cursor, uid, uid).password
                 start = datetime.now()
+                uri = 'http://localhost'
+                if config['secure']:
+                    uri = 'https://localhost'
                 O = OOOP(dbname=cursor.dbname, port=child_port, user=user,
-                         pwd=pwd)
+                         pwd=pwd, uri=uri)
                 obj = Manager(osv_object._name, O)
                 method = f.__name__
                 newargs = args[3:]
