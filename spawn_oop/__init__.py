d038abe358e4a0b358628334d852b931a202ff1a
Eduard Carreras <ecarreras@gisce.net>
2013-05-20 11:39:26 +0200

    Make it compatible to v0.7
    https://code.google.com/p/psutil/source/browse/psutil/error.py?r=e0cfcea0abafddc67cd8cbb893629fc197a48437

--- spawn_oop/__init__.py
+++ spawn_oop/__init__.py
@@ -73,7 +73,7 @@ class spawn(object):
                             % (spawn_proc.pid, spawn_proc.user,
                                spawn_proc.startup)
                         )
-                except psutil.error.NoSuchProcess:
+                except psutil.NoSuchProcess:
                     pass
                 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 sock.bind(('127.0.0.1', 0))
